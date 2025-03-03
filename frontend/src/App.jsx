91399de4edb13d89b61e63fa573d9f77de2b9f75
leontoys <svliyon@gmail.com>
2025-03-03 12:14:53 +1100

    todo-item

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
 
 function App() {
 
   const [todos,setTodos] = useState([])
-  console.log("todos",todos)
 
+  //read from local storage
+  useEffect(()=>{
+    const storedTodos = JSON.parse(localStorage.getItem("todos"))
+    if(storedTodos){
+      setTodos(storedTodos)
+    }
+  },[])
+
+  //update local storage, when todos change
+  useEffect(()=>{
+    if(todos.length > 0){
+    localStorage.setItem("todos",JSON.stringify(todos))
+    }
+  },[todos])
 
   const addTodo = (input)=>{
     //add input to the task list
@@ -22,11 +35,30 @@ function App() {
     setTodos([...todos,newTodo])//add new todo
   }
 
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) =>{
+    setTodos(
+      todos.filter((todo)=>
+        todo.id !== id
+      )
+    )
+  }
+
   return(
     <div>
       <h1>Todo List</h1>
       <TodoForm addTodo={addTodo}></TodoForm>
-      <TodoList todos={todos}></TodoList>
+      <TodoList todos={todos} 
+      toggleComplete={toggleComplete}
+      deleteTodo={deleteTodo}
+      ></TodoList>
     </div>
   )
 }
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,10 +1,14 @@
 import React,{useState} from "react";
+import TodoItem from './TodoItem'
 
-const TodoList = ({todos})=>{
+const TodoList = ({todos,toggleComplete,deleteTodo,handleEdit,handleSave})=>{
     return(
     <ul>
         {todos.map( todo=>
-            <li key={todo.id}>{todo.text}</li>
+            <TodoItem key={todo.id} todo={todo} 
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            ></TodoItem>
         )}
     </ul>
     )
