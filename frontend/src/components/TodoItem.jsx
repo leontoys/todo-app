0ef3f569529a19a6003bd2d94208d2b40086d16b
leontoys <svliyon@gmail.com>
2025-03-03 12:30:13 +1100

    todolist-item-corrected

--- frontend/src/components/TodoItem.jsx
+++ frontend/src/components/TodoItem.jsx
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
-
 const TodoItem = ({todo,toggleComplete,deleteTodo})=>{
-
-    const [isEditing,setIsEditing] = useState(false)
-    const [newText,setNewText] = useState(todo.text)
-
-    const handleEdit = ()=>{
-
-    }
-
-    const handleSave = ()=>{
-
-    }
 
     return(
         <li style={{textDecoration:todo.completed? "line-through":"none",cursor:"pointer"}}>
         <input 
         onChange={()=>toggleComplete(todo.id)}
-        type="checkbox"></input>
-        <input value={todo.text}></input>
-        <button onClick={handleSave} style={{ marginLeft: "8px" }}>💾 Save</button>        
-        <button onClick={handleEdit}>✏️ Edit</button>        
+        type="checkbox"></input>{todo.text}
         <button onClick={()=>deleteTodo(todo.id)}>❌</button>
         </li>
     )
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,11 +1,12 @@
 import React,{useState} from "react";
 import TodoItem from './TodoItem'
 
-const TodoList = ({todos,toggleComplete,deleteTodo,handleEdit,handleSave})=>{
+const TodoList = ({todos,toggleComplete,deleteTodo})=>{
     return(
-    <ul>
+    <ul style={{listStyle:"none"}}>
         {todos.map( todo=>
-            <TodoItem key={todo.id} todo={todo} 
+            <TodoItem key={todo.id} 
+            todo={todo} 
             toggleComplete={toggleComplete}
             deleteTodo={deleteTodo}
             ></TodoItem>
